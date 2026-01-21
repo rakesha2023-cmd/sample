d4ba73d753bf43f6fe0944657e4947cfb28c0f06
Rakesh A <rakesh@gmail.com>
2026-01-21 18:58:47 +0530

    login feature

--- sample.py
+++ sample.py
@@ -3,7 +3,7 @@ library = {}
 print("---- Library Management System ----")
 
 while True:
-    print("\n1.Add  2.Display  3.Issue  4.Return  5.Exit")
+    print("\n1.Add  2.Display  5.Exit")
     ch = input("Enter choice: ")
 
     if ch == "1":
@@ -25,24 +25,7 @@ while True:
                 issued_to = library[b][2] if library[b][1] else "-"
                 print(b, library[b][0], status, issued_to)
 
-    elif ch == "3":
-        bid = input("Enter Book ID to issue: ")
-        if bid in library and not library[bid][1]:
-            student = input("Enter Student Name: ")
-            library[bid][1] = True
-            library[bid][2] = student
-            print("Book issued successfully")
-        else:
-            print("Book not available or not found")
-
-    elif ch == "4":
-        bid = input("Enter Book ID to return: ")
-        if bid in library and library[bid][1]:
-            library[bid][1] = False
-            library[bid][2] = ""
-            print("Book returned successfully")
-        else:
-            print("Invalid book ID or book not issued")
+    
 
     elif ch == "5":
         print("Exiting program...")
